a4f8eaa8cefc8d3622749887d3778272f5fc2c5d
Kimiko <kimikomotoyama@gmail.com>
2016-10-04 18:30:08 +0000

    resume04 company position association

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@ Company.create(name: "株式会社ABC不動産", url: "http://www.abc_sample.co.
 Company.create(name: "株式会社NOWALL", url: "http://nowall.co.jp/", address: "東京都新宿区西新宿6")
 Company.create(name: "株式会社ZZZ運輸", url: "http://www.zzz_sample.co.jp/", address: "東京都新宿区北新宿8")
 
+Post.create(position_name: "社長")
+Post.create(position_name: "部長")
+Post.create(position_name: "課長")
+Post.create(position_name: "平社員")
+
 Faker::Config.locale = :ja
 
 100.times do |index|
@@ -9,6 +14,7 @@ Faker::Config.locale = :ja
     family_name: Faker::Name.last_name,
     given_name: Faker::Name.first_name,
     email: Faker::Internet.email,
-    company_id: rand(1..3)
+    company_id: rand(1..3),
+    post_id: rand(1..4)
   )
 end
